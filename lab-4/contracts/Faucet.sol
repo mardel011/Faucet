f6f5f0e40239fa67dd5cc26c1d0b29d271983fe7
mardel011 <59632640+mardel011@users.noreply.github.com>
2020-05-30 15:02:57 -0400

    Updating files

--- lab-4/contracts/Faucet.sol
+++ lab-4/contracts/Faucet.sol
@@ -1,14 +1,15 @@
+
 // SPDX-License-Identifer: MIT
 
 pragma solidity >0.5.0 <0.6.0;
 
 contract Faucet {
 
-    event Withdrawal(address indexed to, uint amount);
-    event Deposit(address indexed from, uint amount);
+    event Withdrawal(address indexed to, uint256 amount);
+    event Deposit(address indexed from, uint256 amount);
 
     // give out ether to anyone who asks
-    function withdraw(uint withdraw_amount) public {
+    function withdraw(uint256 withdraw_amount) public {
         // check for sufficient funds
         require (withdraw_amount <0.1 ether, "Max withdrawal is 0.1 ether");
         require(address(this).balance >= withdraw_amount, "Faucet: Insufficient balance for withdrawal request");
